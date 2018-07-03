207fbf533863ae4721122b62660f303b8af2f779
Sharroll L Smith <ssmith2@fullsail.edu>
2018-07-03 16:53:33 -0500

    Initial commit

---
+++ Source/Vacation/VacationGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "VacationGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VACATION_API AVacationGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
